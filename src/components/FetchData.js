c6a3a494f523f36946e5102ba7537f14f6d1ddff
Aiste Ulozaite <aiste@jostle.me>
2021-02-21 10:57:13 -0800

    GraphQL example implemented

--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import useFetch from "react-fetch-hook";
 
+import { useQuery, gql } from '@apollo/client';
+
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
+
 const FetchData = () => {
-  const { isLoading, error, data } 
-  = useFetch("https://api.github.com/search/repositories?q=react%20in%3Aname%20in%3Adescription", {
-      headers: {
-        'Accept': 'application/vnd.github.v3+json',
-      }});
-    if (isLoading) return "Loading..."; if (error) return "Error!";
-    return ( <>
-      <pre>{JSON.stringify(data, null, 2)}</pre> </>
-  ); 
+  // const { isLoading, error, data } 
+  // = useFetch("https://api.github.com/search/repositories?q=react%20in%3Aname%20in%3Adescription", {
+  //     headers: {
+  //       'Accept': 'application/vnd.github.v3+json',
+  //     }});
+  //   if (isLoading) return "Loading..."; if (error) return "Error!";
+  //   return ( <>
+  //     <pre>{JSON.stringify(data, null, 2)}</pre> </>
+  // ); 
+
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
+
 }
 
 export default FetchData;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { gql } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
+
+client
+  .query({
+    query: gql`
+      query GetRates {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+
 const render = (Component) => 
-  ReactDOM.render(<App />, document.getElementById('root'));
+  ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider>, document.getElementById('root'));
 
 render(hot(App));
