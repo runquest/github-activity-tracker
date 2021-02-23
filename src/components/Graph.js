de91d1bb945a31066d98fb9699d5487655c6c7f4
Aiste Ulozaite <aiste@jostle.me>
2021-02-22 22:24:04 -0800

    Exploring graph

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -43,16 +43,23 @@ const data = [
 const Graph = () => {
   return (
     <LineChart
-      width={400}
-      height={400}
+      style={container}
+      width={720}
+      height={720}
       data={data}
       margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
     >
       <XAxis dataKey="name" />
+      <YAxis dataKey="uv" />
       <Tooltip />
-      <CartesianGrid stroke="#f5f5f5" />
       <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
-      <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
+      {/* <CartesianGrid stroke="#f5f5f5" /> */}
+
+      {/* 
+      <Tooltip />
+       */}
+      {/* <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} /> */}
+      {/* <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} /> */}
     </LineChart>
   )
 }
