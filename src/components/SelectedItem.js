0b59f27f60cf5f16b43b218966c64026237a992f
Aiste Ulozaite <aiste@jostle.me>
2021-02-23 07:53:28 -0800

    Refactoring and clean up

--- src/components/SelectedItem.js
+++ src/components/SelectedItem.js
@@ -1,13 +1,7 @@
 import React, { useState } from 'react'
-import { Segment, Container } from 'semantic-ui-react'
 
-import FeatherIcon from 'feather-icons-react'
 import ReactTimeAgo from 'react-time-ago'
 import { Trash, Star } from 'react-feather'
-
-const description = {
-  display: 'flex',
-}
 
 const SelectedItem = ({ info, onHover, onClick }) => {
   const [active, setActive] = useState(info.active)
@@ -25,8 +19,9 @@ const SelectedItem = ({ info, onHover, onClick }) => {
   }
 
   return (
-    <Segment
-      className={active ? 'SelectedItem' : 'SelectedItem inactive'}
+    <div
+      className={'SelectedItem'}
+      // className={active ? 'SelectedItem' : 'SelectedItem inactive'}
       style={{ ...customBoxShadow }}
       onMouseEnter={() => {
         setActive(true)
@@ -39,19 +34,23 @@ const SelectedItem = ({ info, onHover, onClick }) => {
     >
       <div className={'SelectedItemMetadata'}>
         <div className={'SelectedItemTopRow'}>
-          <span className={'Owner'}>{info.owner}</span> /
+          <span className={'Owner'}>{info.owner}</span>/
           <span className={'Repo'}>{info.name}</span>
         </div>
         <div className={'SelectedItemBottomRow'}>
-          <Star size={10} color={'#bcbcf2'} />
+          <span>
+            <Star size={14} color={'#bcbcf2'} />
+          </span>
           <span className={'Starred'}>{info.stars.toLocaleString()}k</span>
           <span className={'Updated'}>
             <ReactTimeAgo date={Date.parse(info.updated)} />
           </span>
         </div>
       </div>
-      <Trash style={active ? show : hide} size={14} color={'#fff'} />
-    </Segment>
+      <div style={active ? show : hide}>
+        <Trash size={14} color={'#fff'} />
+      </div>
+    </div>
   )
 }
 
